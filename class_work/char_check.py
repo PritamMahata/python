d278400e4447b28f8157cf44e7249a160fed4585
Pritam <pritammahata12@gmail.com>
2024-01-19 22:21:35 +0530

    rearange and adding some code

---
+++ class_work/char_check.py
@@ -0,0 +1,6 @@
+inp = input("Enter a charecter ")
+if (inp.isalpha()):
+    print("You enter charecter")
+elif (inp.isdigit()):
+    print("You enter digit")
+    
---
+++ class_work/find_large_num.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter 1st number "))
+num2 = int(input("Enter 2nd number "))
+if num1 > num2:
+    print(num1, "is large")
+else:
+    print(num2, "is large")
