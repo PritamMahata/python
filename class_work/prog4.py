439b0d764ef29ebb4ec884db01a8a1c72173c2d1
Pritam <pritammahata12@gmail.com>
2024-01-23 19:35:11 +0530

    rename file and some little modified

--- class_work/prog4.py
+++ class_work/prog4.py
@@ -2,6 +2,17 @@
 name = "Pritam"
 age = 20
 marks = 80.5
+num = 7
+code = 'A'
+pi = 3.1415
+pop_op_India = 100000000
+msg = "Hi"
+
 print("Name:", name)
 print("Age:", age)
 print("Marks:", marks)
+print("Num:", str(num))
+print("Code:",str(code))
+print("Pi:", str(pi))
+print("Population of India:", str(pop_op_India))
+print("Message:", msg)
---
+++ class_work/prog9_avg_4_num.py
@@ -0,0 +1,7 @@
+# WAP to print the average of 4 numbers.
+n1 = int(input("Enter 1st number "))
+n2 = int(input("Enter 2nd number "))
+n3 = int(input("Enter 3rd number "))
+n4 = int(input("Enter 4th number "))
+avg  = n1 + n2 + n3 + n4 / 4
+print("Average of 4 numbers is:", avg)
